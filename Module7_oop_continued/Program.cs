293c911ee67ee2489ce8304a39a554e185e31c56
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-02 19:19:24 +0300

    Задание 7.1.4: Создал 2 наследника класса Employee : ProjectManager и Developer

--- Module7_oop_continued/Program.cs
+++ Module7_oop_continued/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Module7_oop_continued
 {
+    class Employee
+    {
+        public string Name;
+        public int Age;
+        public int Salary;
+    }
+
+    class ProjectManager: Employee
+    {
+        public string ProjectName;
+    }
+
+    class Developer: Employee
+    {
+        public string ProgrammingLanguage;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Developer = new Developer();
+            Developer.ProgrammingLanguage = "C#";
         }
     }
 }
