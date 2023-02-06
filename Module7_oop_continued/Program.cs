d9b66b5b804874b648e4f51c2bea28c5c95aca7d
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-06 20:55:36 +0300

    Задание 7.5.3: Статический метод, который меняет местами значения переменных

--- Module7_oop_continued/Program.cs
+++ Module7_oop_continued/Program.cs
@@ -2,17 +2,27 @@
 
 namespace Module7_oop_continued
 {
-    class Obj
+    class Helper
     {
-        public static int MaxValue = 2000;
-        public string Name;
-        public string Description;
+        public static void Swap(ref int a, ref int b)
+        {
+            int c = b;
+            b = a;
+            a = c;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
+
             Console.ReadLine();
         }
     }
