0e57070d6ef6ccd066d44bbe7f511b24b4129bed
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-06 20:34:36 +0300

    Задание 7.5.2: Создал статическое поле в классе Obj

--- Module7_oop_continued/Program.cs
+++ Module7_oop_continued/Program.cs
@@ -2,46 +2,17 @@
 
 namespace Module7_oop_continued
 {
-    abstract class ComputerPart
+    class Obj
     {
-        public abstract void Work();
-    }
-
-    class Processor : ComputerPart
-    {
-        public override void Work()
-        {
-            Console.WriteLine("Работает процессор");
-        }
-    }
-
-    class MotherBoard : ComputerPart
-    {
-        public override void Work()
-        {
-            Console.WriteLine("Работает материнская карта");
-        }
-    }
-
-    class GraphicCard : ComputerPart
-    {
-        public override void Work()
-        {
-            Console.WriteLine("Используется GPU");
-        }
+        public static int MaxValue = 2000;
+        public string Name;
+        public string Description;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var Gpu = new GraphicCard();
-            var MBoard = new MotherBoard();
-            var Cpu = new Processor();
-
-            Cpu.Work();
-            MBoard.Work();
-            Gpu.Work();
-
+            
             Console.ReadLine();
         }
     }
