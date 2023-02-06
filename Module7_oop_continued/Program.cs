8afe66f0172a7dd6d8bf9924fc503d8642a14f97
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-06 21:19:09 +0300

    Задание 7.5.5: Статический конструктор

--- Module7_oop_continued/Program.cs
+++ Module7_oop_continued/Program.cs
@@ -2,28 +2,33 @@
 
 namespace Module7_oop_continued
 {
-    class Helper
-    {
-        public static void Swap(ref int a, ref int b)
+	class Obj
+	{
+		public string Name;
+		public string Description;
+
+		public static string Parent;
+		public static int DaysInWeek;
+		public static int MaxValue;
+
+		static Obj()
         {
-            int c = b;
-            b = a;
-            a = c;
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int num1 = 3;
-            int num2 = 58;
+			Parent = "System.Object";
+			DaysInWeek = 7;
+			MaxValue = 2000;
+            Console.WriteLine("Конструктор");
+		}
+	}
 
-            Helper.Swap(ref num1, ref num2);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine(Obj.Parent);
+			Console.WriteLine(Obj.DaysInWeek);
+			Console.WriteLine(Obj.MaxValue);
 
-            Console.WriteLine(num1); //58
-            Console.WriteLine(num2); //3
-
-            Console.ReadLine();
-        }
-    }
+			Console.ReadKey();
+		}
+	}
 }
