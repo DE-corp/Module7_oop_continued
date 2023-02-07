555fa0a663d64aa27bde6fe29c4737f798ee0f07
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-02-07 21:59:15 +0300

    Задание 7.6.2: Создал Generic класс

--- Module7_oop_continued/Program.cs
+++ Module7_oop_continued/Program.cs
@@ -2,43 +2,30 @@
 
 namespace Module7_oop_continued
 {
-	static class IntExtensions
-	{
-		public static int GetNegative(this int value)
-		{
-            if (value >= 0)
-            {
-				return 0 - value;
-            }
-            else { return value; }
-		}
+	class Car<T>
+    {
+		public T Engine;
 
-		public static int GetPositive(this int value)
-		{
-			if (value <= 0)
-			{
-				return 0 - value;
-			}
-			else { return value; }
-		}
 	}
 
-	
+	class ElectricEngine
+    {
+
+    }
+
+	class GasEngine
+    {
+		public string str = "Бензин";
+    }
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			int num1 = 7;
-			int num2 = -13;
-			int num3 = 0;
+			var car = new Car<GasEngine>();
+			var car2 = new Car<ElectricEngine>();
 
-			Console.WriteLine(num1.GetNegative()); //-7
-			Console.WriteLine(num1.GetPositive()); //7
-			Console.WriteLine(num2.GetNegative()); //-13
-			Console.WriteLine(num2.GetPositive()); //13
-			Console.WriteLine(num3.GetNegative()); //0
-			Console.WriteLine(num3.GetPositive()); //0
+            Console.ReadLine();
 		}
 	}
 
